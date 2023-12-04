ce7ccd50c5365beda7c04684e811992da83fb980
Roberta D'Agostino <107399627+robdgs@users.noreply.github.com>
2023-12-04 16:58:31 +0100

    ex3

---
+++ C03/ex03/ft_strncat.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rd-agost <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/23 10:19:16 by rd-agost          #+#    #+#             */
+/*   Updated: 2023/11/23 10:39:31 by rd-agost         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0' && j < nb)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+//#include <stdio.h>
+//int	main()
+//{
+//	char src[] = "Piscine";
+//	char dest[] = "42 ";
+//	printf("%s", ft_strncat(dest, src, 7));
+//}
